0940d37e81f728118b34828f48f69b8df16f6d75
jack <hongmomanu@163.com>
2015-09-09 11:34:07 +0800

    jack commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,90 +1,86 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state,$ionicPopup,userService) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $state, $ionicPopup, userService) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  // Form data for the login modal
-  $scope.user = {};
+        // Form data for the login modal
+        $scope.user = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.logout = function() {
-    //$scope.modal.show();
-    //alert(1);
+        // Open the login modal
+        $scope.logout = function () {
+            //$scope.modal.show();
+            //alert(1);
 
-      var confirmPopup = $ionicPopup.confirm({
-        title: '提示',
-        template: '你确定要退出应用么?'
-      });
-      confirmPopup.then(function(res) {
-        if(res) {
-          //console.log('You are sure');
-          $state.go('app');
-        } else {
-          //console.log('You are not sure');
-        }
-      });
-
-
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.signIn = function() {
-      userService.userlogin($scope.user.username,$scope.user.password).then(function(response){
-          if(response.data.success){
-              $state.go('index.search');
-          }else{
-              var alertPopup = $ionicPopup.alert({
-                  title: '登录提示',
-                  template: '用户名或密码错误'
-              });
-              alertPopup.then(function(res) {
-                  //console.log(res);
-              });
-          }
-      });
+            var confirmPopup = $ionicPopup.confirm({
+                title: '提示',
+                template: '你确定要退出应用么?'
+            });
+            confirmPopup.then(function (res) {
+                if (res) {
+                    //console.log('You are sure');
+                    $state.go('app');
+                } else {
+                    //console.log('You are not sure');
+                }
+            });
 
 
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.signIn = function () {
+            userService.userlogin($scope.user.username, $scope.user.password).then(function (response) {
+                if (response.data.success) {
+                    $state.go('index.search');
+                } else {
+                    var alertPopup = $ionicPopup.alert({
+                        title: '登录提示',
+                        template: '用户名或密码错误'
+                    });
+                    alertPopup.then(function (res) {
+                        //console.log(res);
+                    });
+                }
+            });
 
 
-  };
+        };
 
 
-
-})
-
+    })
 
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('PlaylistsCtrl', function ($scope) {
+        $scope.playlists = [
+            {title: 'Reggae', id: 1},
+            {title: 'Chill', id: 2},
+            {title: 'Dubstep', id: 3},
+            {title: 'Indie', id: 4},
+            {title: 'Rap', id: 5},
+            {title: 'Cowbell', id: 6}
+        ];
+    })
 
-.controller('PlaylistCtrl', function($scope, $stateParams,$http) {
+    .controller('PlaylistCtrl', function ($scope, $stateParams, $http) {
 
-      testobj=$http;
-      console.log(22);
-});
+        testobj = $http;
+        console.log(22);
+    });
--- www/js/newcarecontroller.js
+++ www/js/newcarecontroller.js
@@ -1,110 +1,107 @@
 angular.module('main.newcarecontroller', [])
 
-.controller('NewCareCtrl', function($scope, $ionicModal, $timeout, $state,$ionicPopup) {
+    .controller('NewCareCtrl', function ($scope, $ionicModal, $timeout, $state, $ionicPopup) {
 
-  $scope.newcarenocardnext=function(user){
+        $scope.newcarenocardnext = function (user) {
 
-    console.log(user);
-    $state.go('index.selecttemp',user);
+            console.log(user);
+            $state.go('index.selecttemp', user);
 
-  };
+        };
 
-   $scope.newcarecardnext=function(user){
-       console.log(user);
-       //alert(11);
-       $state.go('index.selecttemp', user);
-    //$state.go('index.choosetemp');
-  };
+        $scope.newcarecardnext = function (user) {
+            console.log(user);
+            //alert(11);
+            $state.go('index.selecttemp', user);
+            //$state.go('index.choosetemp');
+        };
 
-    $scope.demo = 'nocard';
-    $scope.newCare = function(p) {
-        document.body.classList.remove('platform-card');
-        document.body.classList.remove('platform-nocard');
-        document.body.classList.add('platform-' + p);
-        $scope.demo = p;
-        if(p=='nocard'){
-            $state.go('index.newcareunknwon');
-        }else{
-            $state.go('index.newcarewithcard');
+        $scope.demo = 'nocard';
+        $scope.newCare = function (p) {
+            document.body.classList.remove('platform-card');
+            document.body.classList.remove('platform-nocard');
+            document.body.classList.add('platform-' + p);
+            $scope.demo = p;
+            if (p == 'nocard') {
+                $state.go('index.newcareunknwon');
+            } else {
+                $state.go('index.newcarewithcard');
+            }
         }
-    }
 
 
-})
-.controller('ChooseTempCtrl',function($scope, $stateParams,$http,$ionicLoading,tempService,$rootScope){
+    })
+    .controller('ChooseTempCtrl', function ($scope, $stateParams, $http, $ionicLoading, tempService, $rootScope) {
         console.log($stateParams);
 
         $rootScope.$on('tempinit', function (event, otherscope) {
-            otherscope.carerecord=$stateParams;
+            otherscope.carerecord = $stateParams;
         });
 
-        tempService.getCaretemp().then(function(response){
-            $scope.caretemplists=response.data;
+        tempService.getCaretemp().then(function (response) {
+            $scope.caretemplists = response.data;
         });
 
-})
-.controller('CaretemplistCtrl', function($scope, $stateParams,$http,$ionicLoading,tempService,$rootScope) {
+    })
+    .controller('CaretemplistCtrl', function ($scope, $stateParams, $http, $ionicLoading, tempService, $rootScope) {
 
-    testobj=$http;
-    console.log($stateParams);
-    tempService.getCaretempByid($stateParams.sigleId).then(function(response){
-        $scope.devList=response.data.content;
-    });
-    $rootScope.$broadcast('tempinit', $scope);
-
-
-
-}).controller('CaredetailsCtrl', function($scope,tempService) {
+        testobj = $http;
+        console.log($stateParams);
+        tempService.getCaretempByid($stateParams.sigleId).then(function (response) {
+            $scope.devList = response.data.content;
+        });
+        $rootScope.$broadcast('tempinit', $scope);
 
 
-        tempService.getRecordByPage(1,10).then(function(response){
-            $scope.caredetails=response.data;
+    }).controller('CaredetailsCtrl', function ($scope, tempService) {
+
+
+        tempService.getRecordByPage(1, 10).then(function (response) {
+            $scope.caredetails = response.data;
         });
 
-        $scope.doRefresh=function(){
+        $scope.doRefresh = function () {
             //alert(1);
-            tempService.getRecordByPage(1,10).then(function(response){
-                $scope.caredetails=response.data;
+            tempService.getRecordByPage(1, 10).then(function (response) {
+                $scope.caredetails = response.data;
                 $scope.$broadcast("scroll.refreshComplete");
             });
         };
 
 
-
     })
 
-.controller('CaredetailCtrl', function($scope, $stateParams,tempService,$state,$ionicPopup,$ionicLoading) {
-
+    .controller('CaredetailCtrl', function ($scope, $stateParams, tempService, $state, $ionicPopup, $ionicLoading) {
 
 
         $scope.clientSideList = [
-            { text: "主体", value: "主体" },
-            { text: "客体", value: "客体" }
+            {text: "主体", value: "主体"},
+            {text: "客体", value: "客体"}
         ];
-        tempService.getRecordById($stateParams.caredetailId).then(function(response){
-            $scope.savedata=response.data;
+        tempService.getRecordById($stateParams.caredetailId).then(function (response) {
+            $scope.savedata = response.data;
             /*$scope.devList=response.data.tempcontent;
-            $scope.caredetail=response.data.caredetail;*/
-            if(!$scope.savedata.caredetail.clientSide)$scope.savedata.caredetail.clientSide='主体'
+             $scope.caredetail=response.data.caredetail;*/
+            if (!$scope.savedata.caredetail.clientSide)$scope.savedata.caredetail.clientSide = '主体'
 
         });
 
-    //testobj=$http;
+        //testobj=$http;
 
-        $scope.playvideo=function(){
+        $scope.playvideo = function () {
             alert(11);
 
         };
-        $scope.saverecord=function(){
+        $scope.saverecord = function () {
 
-            var data=angular.copy($scope.savedata);
+            var data = angular.copy($scope.savedata);
             delete  data._id;
             delete data.time;
-            tempService.saveRecordById($stateParams.caredetailId,data).then(function(response){
-                if(response.data.success){
+            tempService.saveRecordById($stateParams.caredetailId, data).then(function (response) {
+                if (response.data.success) {
                     //$state.go('index.caredetails');
-                    $ionicLoading.show({ template: '表单保存完毕', duration: 1500});
-                }else{
+                    $ionicLoading.show({template: '表单保存完毕', duration: 1500});
+                } else {
                     var alertPopup = $ionicPopup.alert({
                         title: '错误提示',
                         template: '提交数据失败'
@@ -116,35 +113,35 @@ angular.module('main.newcarecontroller', [])
 
         };
 
-})
-.directive('formManager', function($ionicLoading) {
+    })
+    .directive('formManager', function ($ionicLoading) {
         return {
-            restrict : 'A',
-            controller : function($scope,$state,$ionicHistory,$ionicPopup,tempService) {
+            restrict: 'A',
+            controller: function ($scope, $state, $ionicHistory, $ionicPopup, tempService) {
 
-                $scope.$watch('newCareForm.$valid', function() {
+                $scope.$watch('newCareForm.$valid', function () {
                     console.log("Form validity changed. Now : " + $scope.newCareForm.$valid);
                 });
 
-                $scope.savecare = function() {
+                $scope.savecare = function () {
 
-                    if($scope.newCareForm.$valid) {
+                    if ($scope.newCareForm.$valid) {
                         //$scope.finalSubmit();
                         //alert(1)
-                        console.log( $scope.devList);
-                        console.log( $scope.carerecord);
-                        $ionicLoading.show({ template: '数据提交中...'});
-                        $scope.carerecord['tempcontent']=$scope.devList;
-                        $scope.carerecord['caredetail']={};
+                        console.log($scope.devList);
+                        console.log($scope.carerecord);
+                        $ionicLoading.show({template: '数据提交中...'});
+                        $scope.carerecord['tempcontent'] = $scope.devList;
+                        $scope.carerecord['caredetail'] = {};
                         //$scope.carerecord['time']=new Date();
-                        tempService.addnewRecord($scope.carerecord).then(function(response){
+                        tempService.addnewRecord($scope.carerecord).then(function (response) {
                             $ionicLoading.hide();
-                            if(response.data.success){
+                            if (response.data.success) {
                                 $ionicHistory.nextViewOptions({
                                     disableBack: true
                                 });
-                                $state.go('index.singlecare',{caredetailId:response.data.id})
-                            }else{
+                                $state.go('index.singlecare', {caredetailId: response.data.id})
+                            } else {
                                 var alertPopup = $ionicPopup.alert({
                                     title: '错误提示',
                                     template: '提交数据失败'
@@ -153,7 +150,7 @@ angular.module('main.newcarecontroller', [])
                         });
 
                     } else {
-                        $ionicLoading.show({ template: '表单未填写完毕', duration: 1500})
+                        $ionicLoading.show({template: '表单未填写完毕', duration: 1500})
                     }
 
 
@@ -161,16 +158,16 @@ angular.module('main.newcarecontroller', [])
             }
         }
     })
- .directive('textarea', function() {
+    .directive('textarea', function () {
         return {
             restrict: 'E',
-            link: function(scope, element, attr){
-                var update = function(){
+            link: function (scope, element, attr) {
+                var update = function () {
                     element.css("height", "auto");
                     var height = element[0].scrollHeight;
                     element.css("height", element[0].scrollHeight + "px");
                 };
-                scope.$watch(attr.ngModel, function(){
+                scope.$watch(attr.ngModel, function () {
                     update();
                 });
             }
@@ -178,9 +175,6 @@ angular.module('main.newcarecontroller', [])
     });
 
 
-/*.controller('CaretemplistsCtrl', function($scope) {
-
-})*/
 
 
 
